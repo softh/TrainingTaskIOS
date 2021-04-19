696c9f676f9c78987aa785b1a03ea30968effce4
Ramashka_AE <ramashka_ay@st.by>
2021-04-19 22:24:18 +0300

    missing commit

--- SDK/sdk/tools/src/iosMain/kotlin/by/st/kmm/sdk/tools/concurrency/Concurrency.kt
+++ SDK/sdk/tools/src/iosMain/kotlin/by/st/kmm/sdk/tools/concurrency/Concurrency.kt
@@ -1,4 +1,4 @@
-package by.st.crypto.tools.concurrency
+package by.st.kmm.sdk.tools.concurrency
 
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Runnable
@@ -7,9 +7,9 @@ import platform.darwin.dispatch_get_main_queue
 import platform.darwin.dispatch_queue_t
 import kotlin.coroutines.CoroutineContext
 
-internal actual val MainDispatcher: CoroutineDispatcher = NsQueueDispatcher(dispatch_get_main_queue())
+actual val MainDispatcher: CoroutineDispatcher = NsQueueDispatcher(dispatch_get_main_queue())
 
-internal actual val BackgroundDispatcher: CoroutineDispatcher = MainDispatcher
+actual val BackgroundDispatcher: CoroutineDispatcher = MainDispatcher
 
 internal class NsQueueDispatcher(
     private val dispatchQueue: dispatch_queue_t
